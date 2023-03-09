d5edd0db36431366f0df5b2ca621b186c796ad1a
Srikanth Reddy <srikanthreddy2089@gmail.com>
2023-03-10 02:16:09 +0530

    renaming file

--- interview/leetcode/1.two_sum.py
+++ interview/leetcode/1.two_sum.py
@@ -4,7 +4,7 @@ def two_sum(nums, target):
     hash_map = {}
     for pos in range(len(nums)):
         if nums[pos] in hash_map:
-            return hash_map[nums[pos]],pos
+            return [hash_map[nums[pos]],pos]
         check_for = target - nums[pos]
         hash_map[check_for] = pos
 print(two_sum(nums = [2,7,11,15], target = 26))

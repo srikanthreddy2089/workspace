2cc894954dc576d85a811387f7467d7acdb7c2f6
Srikanth Reddy <srikanthreddy2089@gmail.com>
2023-03-11 02:11:28 +0530

    fizzbuzz

--- interview/leetcode/412.FizzBizz.py
+++ interview/leetcode/412.FizzBizz.py
@@ -1,2 +1,24 @@
-#class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
+#https://leetcode.com/problems/fizz-buzz/
+"""
+
+"""
+
+def fizzBuzz(n):
+        lis= []
+        for each in range(1,n+1):
+            if each%3==0:
+                if each%5==0:
+                    ele = 'FizzBuzz'
+                else:
+                    ele = 'Fizz'
+            elif each%5==0:
+                ele = 'Buzz'
+            else:
+                ele = str(each)
+            lis.append(ele)
+        return lis
+
+
+
+n=30
+print(fizzBuzz(n))

7d3aa6c9faa87a0ae22137789b634f7c87c55bda
Srikanth Reddy <srikanthreddy2089@gmail.com>
2023-03-11 01:38:19 +0530

    adding comment for 387

--- interview/leetcode/387.first_non_repeating_char.py
+++ interview/leetcode/387.first_non_repeating_char.py
@@ -1,5 +1,7 @@
 #https://leetcode.com/problems/first-unique-character-in-a-string/
 """
+Iterate each letter and if its first time then add into dict(key is letter, value is position),
+and also append to new string. If any letter is found in dict then remove the letter in new string. 
 """
 
 def firstUniqChar(s):

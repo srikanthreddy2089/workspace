4ca983fdffae11926741e305618658f44393fe11
Srikanth Reddy <srikanthreddy2089@gmail.com>
2023-03-15 02:22:29 +0530

    varibale name change

--- interview/leetcode/223.rectangle_area.py
+++ interview/leetcode/223.rectangle_area.py
@@ -3,13 +3,12 @@
 If overlap, the remove the common area from the area of two rectangles.
 """
 
-def computeArea(self, A, B, C, D, E, F, G, H):
-    area1 = abs(C-A)*abs(B-D)
-    area2 = abs(E-G)*abs(F-H)
-    w = min(C,G)-max(A,E)
-    h = min(D, H)-max(B,F)
+def computeArea(self, ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
+    area1 = abs(ax2-ax1)*abs(ay1-ay2)
+    area2 = abs(bx1-bx2)*abs(by1-by2)
+    w = min(ax2,bx2)-max(ax1,bx1)
+    h = min(ay2, by2)-max(ay1,by1)
     if w<=0 or h<=0:
         return area1 + area2
     else:
         return area1 + area2 - w*h
-

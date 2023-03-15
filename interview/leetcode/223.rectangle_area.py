54568e877e963ae9934a519be37e2f3ac673fac6
Srikanth Reddy <srikanthreddy2089@gmail.com>
2023-03-15 15:45:09 +0530

    rectanlge overlap

--- interview/leetcode/223.rectangle_area.py
+++ interview/leetcode/223.rectangle_area.py
@@ -3,7 +3,7 @@
 If overlap, the remove the common area from the area of two rectangles.
 """
 
-def computeArea(self, ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
+def computeArea(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
     area1 = abs(ax2-ax1)*abs(ay1-ay2)
     area2 = abs(bx1-bx2)*abs(by1-by2)
     w = min(ax2,bx2)-max(ax1,bx1)
@@ -12,3 +12,5 @@ def computeArea(self, ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
         return area1 + area2
     else:
         return area1 + area2 - w*h
+
+print(computeArea(ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0, by1 = -1, bx2 = 9, by2 = 2))
